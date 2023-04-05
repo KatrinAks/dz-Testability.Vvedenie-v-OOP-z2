3856be8049d0da7efbcdb4f9752b8f2307645c8c
kat test <e_aksenova80@mail.ru>
2023-04-05 13:34:07 +0300

    Main.java added

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,11 +1,11 @@
 public class BmiService {
 
-    public int calculate(int kg, int height) {
+    public int calculate(int kg, double meter) {
         int index = 0; // индекс массы тела
         kg = 0; //вес в килограммах
-        double a = 0;
-        double b = 2;
-        height =(int) Math.pow (a, b);
+        meter = 0;
+        double degree = 2;
+        int height =(int) Math.pow (meter, degree);
 
 
         if (index > 0) {
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int kg = 56;
-        double a = 1.65;
-
-        int mass = service.calculate(56, (int)1.65);
+        int mass = service.calculate(56, 1.65);
 
         System.out.println("Индекс массы тела" + mass);
     }
