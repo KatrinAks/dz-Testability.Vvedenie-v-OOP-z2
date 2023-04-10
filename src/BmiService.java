71891bc87f12234d8a9f01790465e30dd440fcb0
kat test <e_aksenova80@mail.ru>
2023-04-10 13:52:02 +0300

    Main.java added

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,16 +1,11 @@
 public class BmiService {
 
     public int calculate(int kg, double meter) {
-        int index = 0; // индекс массы тела
-        kg = 0; //вес в килограммах
-        meter = 0;
         double degree = 2;
         int height =(int) Math.pow (meter, degree);
 
+        int index = kg / height;
 
-        if (index > 0) {
-            index = kg / height;
-        }
-        return index;
+        return index;}
     }
-}
+
